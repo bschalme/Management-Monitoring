973c292545fceca5156f26d5b7b1e83c599aedfb
Brian Schalme <bschalme@airspeed.ca>
2020-06-19 13:52:11 -0500

    Added Spring Boot Actuator, but with no authentication.

--- src/main/java/ca/airspeed/demo/observability/controller/HomeController.java
+++ src/main/java/ca/airspeed/demo/observability/controller/HomeController.java
@@ -5,13 +5,13 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
-	@RequestMapping(value="/")
+	@GetMapping(value="/")
 	public ModelAndView test(HttpServletResponse response) throws IOException{
 		return new ModelAndView("home");
 	}
